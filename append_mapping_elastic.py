76ed2026a364b2e737434536e8fae13630256fd2
whamsy <waatluri@gmail.com>
2017-03-10 14:06:16 -0500

    Changed append code

--- append_mapping_elastic.py
+++ append_mapping_elastic.py
@@ -1,7 +1,11 @@
-mapping = '{ "create" : { "_index" : "tweetmap", "_type" : "tweetdata"} }'
-with open("output3.json",'r') as fin:
-    with open("output4.json",'w') as fout:
+import json
+count = 1
+mapping = { "create" : { "_index" : "tweetmap", "_type" : "tweetdata"} }
+with open("output1.json",'r') as fin:
+    with open("outputfinal.json",'w') as fout:
         for line in fin:
             if line != "\n":
-                print(mapping,file=fout)
+                mapping["create"]["_id"] = count
+                count += 1
+                print(json.dumps(mapping),file=fout)
                 fout.write(line)
