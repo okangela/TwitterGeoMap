ce966dd75d90def7ef3ca59ae4bd5f0dfd410cd8
whamsy <waatluri@gmail.com>
2017-03-11 17:25:41 -0500

    Data Capture from Backend successfull

--- pro/pro/views.py
+++ pro/pro/views.py
@@ -4,6 +4,7 @@ from elasticsearch import Elasticsearch
 import json
 from django.http import JsonResponse
 import requests
+from django.core import serializers
 
 def index(request):
     return render(request, 'index.html')
@@ -19,10 +20,10 @@ def search(request):
         r = requests.get('http://localhost:9200/tweetmap/tweetdata/'+str(i))
         parsed_json = json.loads(r.content)
         outarray.append([str(parsed_json['_source']['geo']['coordinates'][0])+','+str(parsed_json['_source']['geo']['coordinates'][1]),parsed_json['_source']['text'],parsed_json['_source']['user']['name']])
-        print(str(parsed_json['_source']['geo']['coordinates'][0])+','+str(parsed_json['_source']['geo']['coordinates'][1]))
-        print(parsed_json['_source']['text'])
-        print(parsed_json['_source']['user']['name'])
-        # print("success")
+        # print(str(parsed_json['_source']['geo']['coordinates'][0])+','+str(parsed_json['_source']['geo']['coordinates'][1]))
+        # print(parsed_json['_source']['text'])
+        # print(parsed_json['_source']['user']['name'])
+        print("success")
         i = i+1
     # es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
     # reqid = request.GET.get('q', '')
@@ -31,12 +32,16 @@ def search(request):
     # print(" response: '%s'" % (res))
     # print(reqid)
     # jsondata = json.loads(json.dumps(es.get(index='tweetmap', doc_type='tweetdata', id=reqid)))
+    jsondata = json.dumps(outarray)
     # # print(jsondata)
     # # response = HttpResponse(jsondata, content_type="application/json")
     # # print(response)
-    # return JsonResponse(res)
+    return JsonResponse(outarray, safe=False)
 
     # return render('a')
     # return render(request, 'index1.html')
-
-    return render_to_response('index.html', {'array1': outarray})
+    # d = json.dumps(outarray)
+    # data = {'d':d}
+    # return render_to_response('index.html',data)
+    # print(jsondata)
+    # return render_to_response('index.html', {'array1': outarray})
