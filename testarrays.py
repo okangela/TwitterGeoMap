417b8d7f8ea724b1a72f187eed67999553874f4d
whamsy <waatluri@gmail.com>
2017-03-12 00:42:27 -0400

    helper code to troubleshoot

--- testarrays.py
+++ testarrays.py
@@ -1,1 +1,7 @@
-arr_2d = [[]]
+import json
+import requests
+reqid = 'trump'
+r = requests.get('http://localhost:9200/tweetmap/tweetdata/_search?q='+str(reqid))
+parsed_json = json.loads(r.content)
+parsed_json2 = json.dumps(parsed_json['hits']['hits'])
+print(parsed_json2[20])
