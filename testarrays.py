df4ce4c0755b34b9a088f59c3d910c901b8fd6ec
whamsy <waatluri@gmail.com>
2017-03-12 04:28:26 -0400

    Tester piece of code

--- testarrays.py
+++ testarrays.py
@@ -1,7 +1,19 @@
 import json
 import requests
 reqid = 'trump'
-r = requests.get('http://localhost:9200/tweetmap/tweetdata/_search?q='+str(reqid))
+r = requests.get('http://localhost:9200/tweetmap/tweetdata/_search?size=1000&q='+str(reqid))
 parsed_json = json.loads(r.content)
-parsed_json2 = json.dumps(parsed_json['hits']['hits'])
-print(parsed_json2[20])
+hits = parsed_json['hits']['total']
+i = 0
+
+while(i < hits):
+    print(parsed_json['hits']['hits'][i]['_source']['geo']['coordinates'][0])
+    print(parsed_json['hits']['hits'][i]['_source']['geo']['coordinates'][1])
+    print(parsed_json['hits']['hits'][i]['_source']['user']['name'])
+    print(parsed_json['hits']['hits'][i]['_source']['text'])
+
+    # outarray.append([parsed_json['hits']['hits'][i]['_source']['geo']['coordinates'][0],parsed_json['hits']['hits'][i]['_source']['geo']['coordinates'][1],parsed_json['hits']['hits'][i]['_source']['text'],parsed_json['hits']['hits'][i]['_source']['user']['name']])
+
+    i += 1
+# parsed_json2 = json.dumps(parsed_json['hits']['hits'])
+# print(parsed_json['hits']['hits'][3])
